f3ee3a15655ba5ba9e2273b7080bc677b27c014c
vvv <t.moll@outlook.com>
2025-05-31 16:01:12 -0400

    Artist data utility and database scripts

---
+++ RedrawnArtistDataUtils.js
@@ -0,0 +1,23 @@
+import artistsData from './artistData.json';
+const artistImgDir = "./avatars/";
+const artistImgExtension = ".png";
+
+/** Fetches the data for an artist */
+function GetArtistData(artistID) {
+    artistsData.artists.forEach(artistData => {
+        if (artistData.uniqueId == artistID) {
+            return artistData;
+        }
+    });
+
+    return null;
+}
+
+/** Prepares the path string to the artist image. */
+function GetArtistImagePath(artistData) {
+    var areaArtistImage = artistData.name;
+    if (artistData.imageNameOverride === '') {
+        areaArtistImage = artistData.imageNameOverride;
+    }
+    return artistImgDir + areaArtistImage + artistImgExtension;
+}
