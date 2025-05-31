c36b2bb7b6d776a2311ff3561b29d05df1c2726b
vvv <t.moll@outlook.com>
2025-05-31 17:02:40 -0400

    Data now has ids defined; some util updates

--- RedrawnArtistDataUtils.js
+++ RedrawnArtistDataUtils.js
@@ -1,5 +1,5 @@
 //import artistsData from './artistData.json';
-const artistImgDir = "./avatars/";
+const artistImgDir = "https://vulture-boy.github.io/Retro-Redrawn-Data/avatars/";
 const artistImgExtension = ".png";
 
 /** Fetches the data for an artist */
@@ -16,8 +16,11 @@ function GetArtistData(artistID) {
 /** Prepares the path string to the artist image. */
 function GetArtistImagePath(artistData) {
     var areaArtistImage = artistData.name;
-    if (artistData.imageNameOverride === '') {
+    if (!(artistData.imageNameOverride === '')) {
         areaArtistImage = artistData.imageNameOverride;
+    }
+    if (areaArtistImage === '') {
+        return '';
     }
     return artistImgDir + areaArtistImage + artistImgExtension;
 }
