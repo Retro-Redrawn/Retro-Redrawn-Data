ef41fb0d3142dc4096146a4b56d2442465288061
vvv <t.moll@outlook.com>
2025-07-20 16:21:53 -0400

    Fallback profile images

--- ArtistImageLoader.js
+++ ArtistImageLoader.js
@@ -4,18 +4,20 @@
 // Get the DOM container
 // With the artist data list, loop: prep the html base, insert the image URL, name, social link
 
-var artist_container = document.querySelector('#ArtistCollection');
+const artist_container = document.querySelector('#ArtistCollection');
 artist_container.innerHTML = '';
+const unk_img = GetArtistImagePath(GetArtistData('UNDF'));
 
 artists.forEach(artist => {
     // Prepare the HTML block corresponding to an area and its associated credts
+    var art_img = GetArtistImagePath(artist);
+    art_img = art_img == '' ? unk_img : art_img;
     var html = 
     `<div class="col-sm-2">
         <a href="${artist.url}" target="_blank" >
-        <img src="${GetArtistImagePath(artist)}" class="thumb-img">
+        <img src="${art_img}" class="thumb-img" onerror="this.onerror=null; this.src='${unk_img}';" alt="Avatar for ${artist.name}">
         <p class="ArtistName">${artist.name}</h1>
         </a>
     </div>`
     artist_container.innerHTML += html
 });
-
